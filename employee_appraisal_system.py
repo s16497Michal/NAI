1c282275496fb63e31b6971938fd71c94db0d004
michal <michu@sie.skichu>
2020-11-25 18:11:11 +0100

    rules added to control system, basic input - output presentation

--- employee_appraisal_system.py
+++ employee_appraisal_system.py
@@ -16,6 +16,7 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
+
 def enter_a_value(message):
     while True:
         value = input(message)
@@ -34,7 +35,7 @@ efficiency = enter_a_value('Podaj efektywnosc pracownika w skali 1-5: ')
 qualifications = enter_a_value('Podaj poziom kwalifikacji pracownika w skali 1-5: ')
 desire_to_develop = enter_a_value('Podaj poziom chęci rozwoju pracownika w skali 1-5: ')
 
-#funckje ponizej
+# funckje ponizej
 
 efficiency_axis = ctrl.Antecedent(np.arange(0, 6, 1), 'efficiency')
 qualifications_axis = ctrl.Antecedent(np.arange(0, 6, 1), 'qualifications')
@@ -49,20 +50,38 @@ desire_to_develop_axis.automf(5)
 grade_names = ['slaby', 'przecietny', 'sredni', 'przyzwoity', 'dobry']
 grade.automf(names=grade_names)
 
-#rules
+# rules
 
-rule1 = ctrl.Rule(efficiency_axis['poor'] & desire_to_develop_axis['poor'], grade['slaby'])
+rule1 = ctrl.Rule(antecedent=((efficiency_axis['poor'] & qualifications_axis['poor'] & desire_to_develop_axis['poor']) |
+                              (efficiency_axis['poor'] & qualifications_axis['average'] & desire_to_develop_axis['poor']) |
+                              (efficiency_axis['average'] & qualifications_axis['poor'] & desire_to_develop_axis['poor'])),
+                  consequent=(grade['slaby']))
+
+rule2 = ctrl.Rule(antecedent=(efficiency_axis['average'] & qualifications_axis['poor'] & desire_to_develop_axis['average']),
+                  consequent=(grade['przecietny']))
+
+rule3 = ctrl.Rule(antecedent=(efficiency_axis['average'] & qualifications_axis['average'] & desire_to_develop_axis['average']),
+                  consequent=(grade['sredni']))
+
+rule4 = ctrl.Rule(antecedent=((efficiency_axis['average'] & qualifications_axis['good'] & desire_to_develop_axis['average']) |
+                              (efficiency_axis['good'] & qualifications_axis['average'] & desire_to_develop_axis['average'])),
+                  consequent=(grade['przyzwoity']))
+
+rule5 = ctrl.Rule(antecedent=((efficiency_axis['good'] & qualifications_axis['good'] & desire_to_develop_axis['good']) |
+                              (efficiency_axis['good'] & qualifications_axis['average'] & desire_to_develop_axis['good']) |
+                              (efficiency_axis['good'] & qualifications_axis['good'] & desire_to_develop_axis['average'])),
+                  consequent=(grade['dobry']))
 
 efficiency_axis.view()
 
-#test
+# test
 
-grade_control = ctrl.ControlSystem([rule1])
+grade_control = ctrl.ControlSystem(rules=[rule1, rule2, rule3, rule4, rule5])
 
 employee_grade = ctrl.ControlSystemSimulation(grade_control)
 
 employee_grade.input['efficiency'] = efficiency
-#employee_grade.input['qualifications'] = qualifications
+employee_grade.input['qualifications'] = qualifications
 employee_grade.input['desire_to_develop'] = desire_to_develop
 
 employee_grade.compute()
@@ -70,4 +89,3 @@ employee_grade.compute()
 print(employee_grade.output['grade'])
 
 grade.view(sim=employee_grade)
-
